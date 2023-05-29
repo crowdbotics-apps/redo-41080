f3b862bdd8af6104ec2eae4587dd5e95db7bcafb
Crowdbotics <team@crowdbotics.com>
2023-05-29 22:13:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "redo-41080",
+  url: "https://redo-41080.botics.co",
+  api: "https://redo-41080.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
